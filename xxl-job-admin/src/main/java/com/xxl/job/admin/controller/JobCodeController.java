accdcb57046084a42c91950fd96ca915e013fca4
姚琛 <yaochen@beyondcent.com>
2019-06-19 10:05:37 +0800

    GLUE模式接口调整

--- xxl-job-admin/src/main/java/com/xxl/job/admin/controller/JobCodeController.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/controller/JobCodeController.java
@@ -1,5 +1,6 @@
 package com.xxl.job.admin.controller;
 
+import com.xxl.job.admin.controller.annotation.PermessionLimit;
 import com.xxl.job.admin.core.model.XxlJobInfo;
 import com.xxl.job.admin.core.model.XxlJobLogGlue;
 import com.xxl.job.admin.core.util.I18nUtil;
@@ -9,79 +10,104 @@ import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.glue.GlueTypeEnum;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * job code controller
+ *
  * @author xuxueli 2015-12-19 16:13:16
  */
 @Controller
 @RequestMapping("/jobcode")
 public class JobCodeController {
-	
-	@Resource
-	private XxlJobInfoDao xxlJobInfoDao;
-	@Resource
-	private XxlJobLogGlueDao xxlJobLogGlueDao;
 
-	@RequestMapping
-	public String index(Model model, int jobId) {
-		XxlJobInfo jobInfo = xxlJobInfoDao.loadById(jobId);
-		List<XxlJobLogGlue> jobLogGlues = xxlJobLogGlueDao.findByJobId(jobId);
+    @Resource
+    private XxlJobInfoDao xxlJobInfoDao;
+    @Resource
+    private XxlJobLogGlueDao xxlJobLogGlueDao;
 
-		if (jobInfo == null) {
-			throw new RuntimeException(I18nUtil.getString("jobinfo_glue_jobid_unvalid"));
-		}
-		if (GlueTypeEnum.BEAN == GlueTypeEnum.match(jobInfo.getGlueType())) {
-			throw new RuntimeException(I18nUtil.getString("jobinfo_glue_gluetype_unvalid"));
-		}
+    @RequestMapping
+    public String index(Model model, int jobId) {
+        XxlJobInfo jobInfo = xxlJobInfoDao.loadById(jobId);
+        List<XxlJobLogGlue> jobLogGlues = xxlJobLogGlueDao.findByJobId(jobId);
 
-		// Glue类型-字典
-		model.addAttribute("GlueTypeEnum", GlueTypeEnum.values());
+        if (jobInfo == null) {
+            throw new RuntimeException(I18nUtil.getString("jobinfo_glue_jobid_unvalid"));
+        }
+        if (GlueTypeEnum.BEAN == GlueTypeEnum.match(jobInfo.getGlueType())) {
+            throw new RuntimeException(I18nUtil.getString("jobinfo_glue_gluetype_unvalid"));
+        }
 
-		model.addAttribute("jobInfo", jobInfo);
-		model.addAttribute("jobLogGlues", jobLogGlues);
-		return "jobcode/jobcode.index";
-	}
-	
-	@RequestMapping("/save")
-	@ResponseBody
-	public ReturnT<String> save(Model model, int id, String glueSource, String glueRemark) {
-		// valid
-		if (glueRemark==null) {
-			return new ReturnT<String>(500, (I18nUtil.getString("system_please_input") + I18nUtil.getString("jobinfo_glue_remark")) );
-		}
-		if (glueRemark.length()<4 || glueRemark.length()>100) {
-			return new ReturnT<String>(500, I18nUtil.getString("jobinfo_glue_remark_limit"));
-		}
-		XxlJobInfo exists_jobInfo = xxlJobInfoDao.loadById(id);
-		if (exists_jobInfo == null) {
-			return new ReturnT<String>(500, I18nUtil.getString("jobinfo_glue_jobid_unvalid"));
-		}
-		
-		// update new code
-		exists_jobInfo.setGlueSource(glueSource);
-		exists_jobInfo.setGlueRemark(glueRemark);
-		exists_jobInfo.setGlueUpdatetime(new Date());
-		xxlJobInfoDao.update(exists_jobInfo);
+        // Glue类型-字典
+        model.addAttribute("GlueTypeEnum", GlueTypeEnum.values());
 
-		// log old code
-		XxlJobLogGlue xxlJobLogGlue = new XxlJobLogGlue();
-		xxlJobLogGlue.setJobId(exists_jobInfo.getId());
-		xxlJobLogGlue.setGlueType(exists_jobInfo.getGlueType());
-		xxlJobLogGlue.setGlueSource(glueSource);
-		xxlJobLogGlue.setGlueRemark(glueRemark);
-		xxlJobLogGlueDao.save(xxlJobLogGlue);
+        model.addAttribute("jobInfo", jobInfo);
+        model.addAttribute("jobLogGlues", jobLogGlues);
+        return "jobcode/jobcode.index";
+    }
 
-		// remove code backup more than 30
-		xxlJobLogGlueDao.removeOld(exists_jobInfo.getId(), 30);
+    @RequestMapping("/{id}")
+    @ResponseBody
+    @PermessionLimit(limit = false)
+    public ReturnT<String> getCode(@PathVariable("id") Integer jobId) {
+        XxlJobInfo jobInfo = xxlJobInfoDao.loadById(jobId);
+        List<XxlJobLogGlue> jobLogGlues = xxlJobLogGlueDao.findByJobId(jobId);
 
-		return ReturnT.SUCCESS;
-	}
-	
+        if (jobInfo == null) {
+            throw new RuntimeException(I18nUtil.getString("jobinfo_glue_jobid_unvalid"));
+        }
+        if (GlueTypeEnum.BEAN == GlueTypeEnum.match(jobInfo.getGlueType())) {
+            throw new RuntimeException(I18nUtil.getString("jobinfo_glue_gluetype_unvalid"));
+        }
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("GlueTypeEnum", GlueTypeEnum.values());
+        resultMap.put("jobInfo", jobInfo);
+        resultMap.put("jobLogGlues", jobLogGlues);
+        return new ReturnT(resultMap);
+    }
+
+    @RequestMapping("/save")
+    @ResponseBody
+    @PermessionLimit(limit = false)
+    public ReturnT<String> save(Model model, int id, String glueSource, String glueRemark) {
+        // valid
+        if (glueRemark == null) {
+            return new ReturnT<String>(500, (I18nUtil.getString("system_please_input") + I18nUtil.getString("jobinfo_glue_remark")));
+        }
+        if (glueRemark.length() < 4 || glueRemark.length() > 100) {
+            return new ReturnT<String>(500, I18nUtil.getString("jobinfo_glue_remark_limit"));
+        }
+        XxlJobInfo exists_jobInfo = xxlJobInfoDao.loadById(id);
+        if (exists_jobInfo == null) {
+            return new ReturnT<String>(500, I18nUtil.getString("jobinfo_glue_jobid_unvalid"));
+        }
+
+        // update new code
+        exists_jobInfo.setGlueSource(glueSource);
+        exists_jobInfo.setGlueRemark(glueRemark);
+        exists_jobInfo.setGlueUpdatetime(new Date());
+        xxlJobInfoDao.update(exists_jobInfo);
+
+        // log old code
+        XxlJobLogGlue xxlJobLogGlue = new XxlJobLogGlue();
+        xxlJobLogGlue.setJobId(exists_jobInfo.getId());
+        xxlJobLogGlue.setGlueType(exists_jobInfo.getGlueType());
+        xxlJobLogGlue.setGlueSource(glueSource);
+        xxlJobLogGlue.setGlueRemark(glueRemark);
+        xxlJobLogGlueDao.save(xxlJobLogGlue);
+
+        // remove code backup more than 30
+        xxlJobLogGlueDao.removeOld(exists_jobInfo.getId(), 30);
+
+        return ReturnT.SUCCESS;
+    }
+
 }
