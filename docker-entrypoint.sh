23b52db3b3ee33c1846a175178c1b93acae05b91
吴彬 <wubin-oem@beyondcent.com>
2020-07-13 15:03:21 +0800

    refactor: 删除多余代码
    body: 删除多余代码

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,21 +12,6 @@ eval sed -i -e 's/\{data_password\}/${data_password}/' /opt/paas/images/bocloud-
 
 echo "replace {data_prefix} to ${data_prefix}"
 eval sed -i -e 's/\{data_prefix\}/${data_prefix}/' /opt/paas/images/bocloud-task/application.properties
-
-echo "replace {email_host} to ${email_host}"
-eval sed -i -e 's/\{email_host\}/${email_host}/' /opt/paas/images/bocloud-task/application.properties
-
-echo "replace {email_port} to ${email_port}"
-eval sed -i -e 's/\{email_port\}/${email_port}/' /opt/paas/images/bocloud-task/application.properties
-
-echo "replace {email_username} to ${email_username}"
-eval sed -i -e 's/\{email_username\}/${email_username}/' /opt/paas/images/bocloud-task/application.properties
-
-echo "replace {email_password} to ${email_password}"
-eval sed -i -e 's/\{email_password\}/${email_password}/' /opt/paas/images/bocloud-task/application.properties
-
-echo "replace {email_sendnick} to ${email_sendnick}"
-eval sed -i -e 's/\{email_sendnick\}/${email_sendnick}/' /opt/paas/images/bocloud-task/application.properties
 exec "$@"
 
 
