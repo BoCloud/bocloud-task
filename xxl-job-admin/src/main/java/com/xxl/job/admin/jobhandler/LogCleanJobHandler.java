733e9a2e039bc31af38c7592833f44dc2a0a1ef8
liurui <liurui@beyondcent.com>
2021-01-18 16:42:13 +0800

    feature: 增加xxljob日志定时清理任务

---
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/jobhandler/LogCleanJobHandler.java
@@ -0,0 +1,36 @@
+package com.xxl.job.admin.jobhandler;
+
+import com.xxl.job.admin.dao.XxlJobLogDao;
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.handler.IJobHandler;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import com.xxl.job.core.log.XxlJobLogger;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * @author liurui
+ * @date 2021/1/18
+ */
+@Service
+public class LogCleanJobHandler extends IJobHandler {
+
+    @Resource
+    private XxlJobLogDao xxlJobLogDao;
+
+
+    @XxlJob("cleanLogJobHandler")
+    @Override
+    public ReturnT<String> execute(String param) throws Exception {
+        XxlJobLogger.log("clean log begin: " + System.currentTimeMillis());
+        try {
+            xxlJobLogDao.bizCleanLog();
+            return ReturnT.SUCCESS;
+        } catch (Exception e) {
+            XxlJobLogger.log("clean log error: " + e);
+            e.printStackTrace();
+            return new ReturnT<>(ReturnT.FAIL_CODE, "clean log error: " + e.toString());
+        }
+    }
+}
