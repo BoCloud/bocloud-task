7e3a4b03953937d2f72ddd9d05cfe8f2f2879de3
liurui <liurui@beyondcent.com>
2021-01-18 20:17:25 +0800

    fix: 增加配置

---
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/core/conf/XxlJobConfig.java
@@ -0,0 +1,50 @@
+package com.xxl.job.admin.core.conf;
+
+import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author liurui
+ * @date 2021/1/18
+ */
+@Configuration
+@ComponentScan(basePackages = "com.xxl.job.admin.jobhandler")
+public class XxlJobConfig {
+
+    @Value("${server.port:9024}")
+    private Integer serverPort;
+
+    @Value("${xxl.job.executor.ip}")
+    private String ip;
+
+    @Value("${xxl.job.executor.port}")
+    private int port;
+
+    @Value("${xxl.job.accessToken}")
+    private String accessToken;
+
+    @Value("${xxl.job.executor.logpath}")
+    private String logPath;
+
+    @Value("${xxl.job.executor.logretentiondays:3}")
+    private int logRetentionDays;
+
+
+    @Bean(initMethod = "start", destroyMethod = "destroy")
+    public XxlJobSpringExecutor xxlJobExecutor() {
+        XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
+        xxlJobSpringExecutor.setAdminAddresses("http://bocloud-task:" + serverPort + "/bocloud-task");
+        xxlJobSpringExecutor.setAppname("task-bocloud-task");
+        xxlJobSpringExecutor.setIp(ip);
+        xxlJobSpringExecutor.setPort(port);
+        xxlJobSpringExecutor.setAccessToken(accessToken);
+        xxlJobSpringExecutor.setLogPath(logPath);
+        xxlJobSpringExecutor.setLogRetentionDays(logRetentionDays);
+        return xxlJobSpringExecutor;
+    }
+
+
+}
